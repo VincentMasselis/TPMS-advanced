d809b4261352546c379de83bfb9074430a639f4a
Vincent Masselis <vincent.masselis@gmail.com>
2022-06-09 21:47:59 +0200

    Project creation + launcher icon

---
+++ app/src/main/java/com/masselis/tpmsadvanced/interfaces/RootActivity.kt
@@ -0,0 +1,14 @@
+package com.masselis.tpmsadvanced.interfaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+
+class RootActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
