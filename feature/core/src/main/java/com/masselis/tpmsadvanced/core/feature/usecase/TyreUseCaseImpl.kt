519a2bde5bf0ca33fefefa02a4fd37d13df47244
Vincent Masselis <vincent.masselis@gmail.com>
2022-10-17 11:25:42 +0200

    Added a debounce when a value is fetch

--- feature/core/src/main/java/com/masselis/tpmsadvanced/core/feature/usecase/TyreUseCaseImpl.kt
+++ feature/core/src/main/java/com/masselis/tpmsadvanced/core/feature/usecase/TyreUseCaseImpl.kt
@@ -6,8 +6,10 @@ import com.masselis.tpmsadvanced.data.record.interfaces.BluetoothLeScanner
 import com.masselis.tpmsadvanced.data.record.model.SensorLocation
 import com.masselis.tpmsadvanced.data.record.model.Tyre
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.first
@@ -17,7 +19,9 @@ import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.shareIn
 import java.util.*
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.seconds
 
+@OptIn(FlowPreview::class)
 @TyreScope
 public class TyreUseCaseImpl @Inject internal constructor(
     private val carId: UUID,
@@ -30,7 +34,12 @@ public class TyreUseCaseImpl @Inject internal constructor(
     private val flow = flow {
         emit(scanner.highDutyScan().filterLocation().first())
         emitAll(scanner.normalScan().filterLocation())
-    }.onEach { tyre -> tyreDatabase.insert(tyre, carId) }
+    }
+        // When a sensor needs to send a value, it emit the same value 10 times within a 1 second
+        // frame. To avoid saving the same value multiple times into the database, a debounce is
+        // applied to fetch only the latest value.
+        .debounce(1.seconds)
+        .onEach { tyre -> tyreDatabase.insert(tyre, carId) }
         .shareIn(
             scope,
             SharingStarted.WhileSubscribed()
