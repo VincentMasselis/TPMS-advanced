bcba46b3f7b121944b755ed8ee4aadfbbf21b62d
Vincent Masselis <vincent.masselis@gmail.com>
2024-05-21 12:56:15 +0200

    Checks for releases notes before releasing to the play store

--- buildSrc/src/main/kotlin/com/masselis/tpmsadvanced/playstore/PlayStoreExtension.kt
+++ buildSrc/src/main/kotlin/com/masselis/tpmsadvanced/playstore/PlayStoreExtension.kt
@@ -1,7 +1,10 @@
 package com.masselis.tpmsadvanced.playstore
 
+import StricSemanticVersion
 import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
 
 public interface PlayStoreExtension {
+    public val version: Property<StricSemanticVersion>
     public val serviceAccountCredentials: RegularFileProperty
 }
