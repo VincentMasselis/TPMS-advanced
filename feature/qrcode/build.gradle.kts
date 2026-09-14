97baeeccc9af0c0994d28467e27cccfe792ef45c
Paul Willard <paul@paulwillard.nz>
2026-09-14 16:21:14 +1200

    Clean up sensor ID parser tests

--- feature/qrcode/build.gradle.kts
+++ feature/qrcode/build.gradle.kts
@@ -23,6 +23,6 @@ dependencies {
     implementation(libs.androidx.camera2.core)
     implementation(libs.androidx.camera2.lifecycle)
     implementation(libs.androidx.camera2.mlkitvision)
-	
-	testImplementation(project(":core:test"))
+
+    testImplementation(project(":core:test"))
 }
